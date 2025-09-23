1eeba244f4d39d228523408005c5f98554c7f585
Sef-Hk <ahk.sefsaf@gmail.com>
2025-09-24 00:32:58 +0100

    add favicon , try to handle the issue for ios

--- src/app-pdf/_components/ui/flipbook-viewer/flipbook/pdf-page.jsx
+++ src/app-pdf/_components/ui/flipbook-viewer/flipbook/pdf-page.jsx
@@ -2,22 +2,40 @@ import { cn } from "@/app-pdf/_lib/utils";
 import React, { forwardRef, memo } from "react";
 import { Page } from "react-pdf";
 
-const PdfPage = forwardRef(({ page, height, zoomScale, isPageInView, isPageInViewRange }, ref) => {
-    return (
-        // <div ref={ref} className={cn(page % 2 === 0 ? 'bg-transparent ' : 'bg-muted')} >
-           <div ref={ref} className="bg-transparent">
+// const PdfPage = forwardRef(({ page, height, zoomScale, isPageInView, isPageInViewRange }, ref) => {
+//     return (
+//         // <div ref={ref} className={cn(page % 2 === 0 ? 'bg-transparent ' : 'bg-muted')} >
+//            <div ref={ref} className="bg-transparent">
    
+//         {isPageInViewRange && (
+//                 <Page
+//                     devicePixelRatio={(isPageInView && zoomScale > 1.7) ? Math.min(zoomScale * window.devicePixelRatio, 5) : window.devicePixelRatio}
+//                     height={height}
+//                     pageNumber={page}
+//                     loading={<></>}
+//                 />
+//             )}
+//         </div>
+//     );
+// });
+const PdfPage = forwardRef(({ page, height, zoomScale, isPageInView, isPageInViewRange }, ref) => {
+    const safeDPR = Math.min(window.devicePixelRatio || 1, 2); // cap at 2 for iOS Safari
+  
+    return (
+      <div ref={ref} className="bg-transparent">
         {isPageInViewRange && (
-                <Page
-                    devicePixelRatio={(isPageInView && zoomScale > 1.7) ? Math.min(zoomScale * window.devicePixelRatio, 5) : window.devicePixelRatio}
-                    height={height}
-                    pageNumber={page}
-                    loading={<></>}
-                />
-            )}
-        </div>
+          <Page
+            devicePixelRatio={(isPageInView && zoomScale > 1.5)
+              ? Math.min(zoomScale * safeDPR, 3)
+              : safeDPR}
+            height={height}
+            pageNumber={page}
+            loading={<></>}
+          />
+        )}
+      </div>
     );
-});
+  });
 
 PdfPage.displayName = "PdfPage";
 
