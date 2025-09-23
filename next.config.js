99013b6fcadb84e6f8ad79e149994b13a9eb0425
Sef-Hk <ahk.sefsaf@gmail.com>
2025-09-23 03:41:11 +0100

    try fix issue 1

--- next.config.js
+++ next.config.js
@@ -1,23 +1,54 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+// import { withPayload } from '@payloadcms/next/withPayload'
 
+// import redirects from './redirects.js'
+
+// const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+//   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+//   : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+//         const url = new URL(item)
+
+//         return {
+//           hostname: url.hostname,
+//           protocol: url.protocol.replace(':', ''),
+//         }
+//       }),
+//     ],
+//   },
+//   webpack: (webpackConfig) => {
+//     webpackConfig.resolve.extensionAlias = {
+//       '.cjs': ['.cts', '.cjs'],
+//       '.js': ['.ts', '.tsx', '.js', '.jsx'],
+//       '.mjs': ['.mts', '.mjs'],
+//     }
+
+//     return webpackConfig
+//   },
+//   reactStrictMode: true,
+//   redirects,
+// }
+
+// export default withPayload(nextConfig, { devBundleServerPackages: false })
+
+import { withPayload } from '@payloadcms/next/withPayload'
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || process.env.SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
+      {
+        protocol: new URL(NEXT_PUBLIC_SERVER_URL).protocol.replace(':', ''),
+        hostname: new URL(NEXT_PUBLIC_SERVER_URL).hostname,
+      },
     ],
   },
   webpack: (webpackConfig) => {
@@ -26,7 +57,6 @@ const nextConfig = {
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
-
     return webpackConfig
   },
   reactStrictMode: true,
